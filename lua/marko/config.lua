8a85fa73e3e7dcd1bd6979cf79818b4d953b1c75
mohseenrm <mohseenmukaddam6@gmail.com>
2025-03-13 23:25:05 -0700

    update: saving progress

--- lua/marko/config.lua
+++ lua/marko/config.lua
@@ -37,16 +37,78 @@ function M.del_marks(skipped_marks)
 	end
 end
 
--- TODO: group and save marks, return only filtered marks
+-- Function to save the current directory's marks
+function M.save_current_marks()
+	local cwd = vim.fn.getcwd()
+	vim.notify("Saving marks for: " .. cwd, vim.log.levels.INFO, { title = "marko.nvim" })
+
+	-- Use our improved filter_marks function to get current marks
+	local curr_marks = file.filter_marks(cwd)
+
+	-- Debug what we found
+	if #curr_marks > 0 then
+		vim.notify("Found " .. #curr_marks .. " marks to save", vim.log.levels.INFO, { title = "marko.nvim" })
+
+		-- List each mark for debugging
+		for _, mark in ipairs(curr_marks) do
+			local content = vim.api.nvim_get_mark(mark, {})
+			if content and content[4] then
+				vim.notify("Mark " .. mark .. " at " .. content[4], vim.log.levels.DEBUG, { title = "marko.nvim" })
+			end
+		end
+	else
+		vim.notify("No marks found to save", vim.log.levels.WARN, { title = "marko.nvim" })
+	end
+
+	-- Force save all marks for this directory
+	local result = file.save_directory_marks(marks_path, cwd)
+
+	-- Verify what was saved
+	if result and result[cwd] then
+		local saved_count = #result[cwd]
+		vim.notify(
+			"Successfully saved " .. saved_count .. " marks for " .. cwd,
+			vim.log.levels.INFO,
+			{ title = "marko.nvim" }
+		)
+		return true
+	else
+		vim.notify("No marks saved for " .. cwd, vim.log.levels.WARN, { title = "marko.nvim" })
+		return false
+	end
+end
+
 function M.setup()
+	-- Create user command to manually save marks
+	vim.api.nvim_create_user_command("MarkoSave", function()
+		M.save_current_marks()
+	end, { desc = "Save marks for current directory" })
+
+	-- Load marks when Neovim starts and save any current marks
 	vim.api.nvim_create_autocmd("UIEnter", {
 		callback = function()
-			-- INFO: clear marks not part of cwd
-			local config = file.get_config(marks_path)
-			print("CONFIG: " .. vim.inspect(config))
-			local cwd = vim.fn.getcwd()
-			local filtered_marks = M.filter_marks(cwd)
-			print("FILTERED_MARKS 2: " .. vim.inspect(filtered_marks))
+			vim.notify(
+				"Initializing marko.nvim for directory: " .. vim.fn.getcwd(),
+				vim.log.levels.INFO,
+				{ title = "marko.nvim" }
+			)
+
+			-- Force save current marks
+			M.save_current_marks()
+		end,
+	})
+
+	-- Save marks when Neovim exits
+	vim.api.nvim_create_autocmd("VimLeavePre", {
+		callback = function()
+			M.save_current_marks()
+		end,
+	})
+
+	-- Also save marks on buffer write
+	vim.api.nvim_create_autocmd("BufWritePost", {
+		callback = function()
+			M.save_current_marks()
 		end,
 	})
 end
